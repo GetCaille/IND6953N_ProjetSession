5dbc80b674f1535ac9d29e68fb17f34711d45ea9
GetCaille <57194101+GetCaille@users.noreply.github.com>
2020-04-14 20:39:49 -0400

    main

---
+++ Main.py
@@ -0,0 +1,6 @@
+#  Main file for application
+# We ask the user to select a few compagnie
+
+ticker1 = input("What is the ticker of the compagnie")
+ticker2 = input("What is the ticker of the compagnie")
+
